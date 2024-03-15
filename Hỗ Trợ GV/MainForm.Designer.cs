627872a13a6c404a1bea0d867805234839cd63b4
mk <minhkhanh04082004@gmail.com>
2024-03-15 19:12:09 +0700

    Thêm 3 form

--- Hỗ Trợ GV/MainForm.Designer.cs
+++ Hỗ Trợ GV/MainForm.Designer.cs
@@ -28,12 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.gunaGradientPanel1 = new Guna.UI.WinForms.GunaGradientPanel();
+            this.btn_dangxuat = new Guna.UI.WinForms.GunaGradientButton();
+            this.lb_HoTro = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_thongke = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_tinhluong = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_lichday = new Guna.UI2.WinForms.Guna2Button();
+            this.pn_main = new System.Windows.Forms.Panel();
+            this.gunaGradientPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gunaGradientPanel1
+            // 
+            this.gunaGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("gunaGradientPanel1.BackgroundImage")));
+            this.gunaGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.gunaGradientPanel1.Controls.Add(this.btn_dangxuat);
+            this.gunaGradientPanel1.Controls.Add(this.lb_HoTro);
+            this.gunaGradientPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gunaGradientPanel1.GradientColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.gunaGradientPanel1.GradientColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.gunaGradientPanel1.GradientColor3 = System.Drawing.Color.White;
+            this.gunaGradientPanel1.GradientColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.gunaGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.gunaGradientPanel1.Name = "gunaGradientPanel1";
+            this.gunaGradientPanel1.Size = new System.Drawing.Size(1220, 34);
+            this.gunaGradientPanel1.TabIndex = 0;
+            this.gunaGradientPanel1.Text = "gunaGradientPanel1";
+            // 
+            // btn_dangxuat
+            // 
+            this.btn_dangxuat.AnimationHoverSpeed = 0.07F;
+            this.btn_dangxuat.AnimationSpeed = 0.03F;
+            this.btn_dangxuat.BackColor = System.Drawing.Color.Transparent;
+            this.btn_dangxuat.BaseColor1 = System.Drawing.Color.White;
+            this.btn_dangxuat.BaseColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btn_dangxuat.BorderColor = System.Drawing.Color.Blue;
+            this.btn_dangxuat.BorderSize = 1;
+            this.btn_dangxuat.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btn_dangxuat.FocusedColor = System.Drawing.Color.Empty;
+            this.btn_dangxuat.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dangxuat.ForeColor = System.Drawing.Color.Black;
+            this.btn_dangxuat.Image = null;
+            this.btn_dangxuat.ImageSize = new System.Drawing.Size(20, 20);
+            this.btn_dangxuat.Location = new System.Drawing.Point(1124, 2);
+            this.btn_dangxuat.Name = "btn_dangxuat";
+            this.btn_dangxuat.OnHoverBaseColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_dangxuat.OnHoverBaseColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btn_dangxuat.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btn_dangxuat.OnHoverForeColor = System.Drawing.Color.Black;
+            this.btn_dangxuat.OnHoverImage = null;
+            this.btn_dangxuat.OnPressedColor = System.Drawing.Color.Black;
+            this.btn_dangxuat.Radius = 5;
+            this.btn_dangxuat.Size = new System.Drawing.Size(93, 31);
+            this.btn_dangxuat.TabIndex = 1;
+            this.btn_dangxuat.Text = "Đăng xuất";
+            this.btn_dangxuat.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lb_HoTro
+            // 
+            this.lb_HoTro.AutoSize = true;
+            this.lb_HoTro.BackColor = System.Drawing.Color.Transparent;
+            this.lb_HoTro.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_HoTro.Location = new System.Drawing.Point(3, 5);
+            this.lb_HoTro.Name = "lb_HoTro";
+            this.lb_HoTro.Size = new System.Drawing.Size(153, 24);
+            this.lb_HoTro.TabIndex = 1;
+            this.lb_HoTro.Text = "Hỗ trợ giảng viên";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btn_thongke);
+            this.panel1.Controls.Add(this.btn_tinhluong);
+            this.panel1.Controls.Add(this.btn_lichday);
+            this.panel1.Location = new System.Drawing.Point(0, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1220, 49);
+            this.panel1.TabIndex = 1;
+            // 
+            // btn_thongke
+            // 
+            this.btn_thongke.BackColor = System.Drawing.Color.Transparent;
+            this.btn_thongke.CheckedState.Parent = this.btn_thongke;
+            this.btn_thongke.CustomImages.Parent = this.btn_thongke;
+            this.btn_thongke.FillColor = System.Drawing.Color.Transparent;
+            this.btn_thongke.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btn_thongke.ForeColor = System.Drawing.Color.Black;
+            this.btn_thongke.HoverState.Parent = this.btn_thongke;
+            this.btn_thongke.Location = new System.Drawing.Point(324, 1);
+            this.btn_thongke.Name = "btn_thongke";
+            this.btn_thongke.ShadowDecoration.Parent = this.btn_thongke;
+            this.btn_thongke.Size = new System.Drawing.Size(156, 45);
+            this.btn_thongke.TabIndex = 4;
+            this.btn_thongke.Text = "Thống kê";
+            this.btn_thongke.Click += new System.EventHandler(this.btn_thongke_Click);
+            // 
+            // btn_tinhluong
+            // 
+            this.btn_tinhluong.BackColor = System.Drawing.Color.Transparent;
+            this.btn_tinhluong.CheckedState.Parent = this.btn_tinhluong;
+            this.btn_tinhluong.CustomImages.Parent = this.btn_tinhluong;
+            this.btn_tinhluong.FillColor = System.Drawing.Color.Transparent;
+            this.btn_tinhluong.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btn_tinhluong.ForeColor = System.Drawing.Color.Black;
+            this.btn_tinhluong.HoverState.Parent = this.btn_tinhluong;
+            this.btn_tinhluong.Location = new System.Drawing.Point(162, 1);
+            this.btn_tinhluong.Name = "btn_tinhluong";
+            this.btn_tinhluong.ShadowDecoration.Parent = this.btn_tinhluong;
+            this.btn_tinhluong.Size = new System.Drawing.Size(156, 45);
+            this.btn_tinhluong.TabIndex = 3;
+            this.btn_tinhluong.Text = "Tính lương";
+            this.btn_tinhluong.Click += new System.EventHandler(this.btn_tinhluong_Click);
+            // 
+            // btn_lichday
+            // 
+            this.btn_lichday.BackColor = System.Drawing.Color.Transparent;
+            this.btn_lichday.CheckedState.Parent = this.btn_lichday;
+            this.btn_lichday.CustomImages.Parent = this.btn_lichday;
+            this.btn_lichday.FillColor = System.Drawing.Color.Transparent;
+            this.btn_lichday.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btn_lichday.ForeColor = System.Drawing.Color.Black;
+            this.btn_lichday.HoverState.Parent = this.btn_lichday;
+            this.btn_lichday.Location = new System.Drawing.Point(0, 1);
+            this.btn_lichday.Name = "btn_lichday";
+            this.btn_lichday.ShadowDecoration.Parent = this.btn_lichday;
+            this.btn_lichday.Size = new System.Drawing.Size(156, 45);
+            this.btn_lichday.TabIndex = 2;
+            this.btn_lichday.Text = "Lịch dạy";
+            this.btn_lichday.Click += new System.EventHandler(this.btn_lichday_Click);
+            // 
+            // pn_main
+            // 
+            this.pn_main.BackColor = System.Drawing.Color.Gainsboro;
+            this.pn_main.Location = new System.Drawing.Point(0, 82);
+            this.pn_main.Name = "pn_main";
+            this.pn_main.Size = new System.Drawing.Size(1220, 630);
+            this.pn_main.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1220, 710);
+            this.Controls.Add(this.pn_main);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.gunaGradientPanel1);
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.gunaGradientPanel1.ResumeLayout(false);
+            this.gunaGradientPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Guna.UI.WinForms.GunaGradientPanel gunaGradientPanel1;
+        private Guna.UI.WinForms.GunaGradientButton btn_dangxuat;
+        private System.Windows.Forms.Label lb_HoTro;
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI2.WinForms.Guna2Button btn_thongke;
+        private Guna.UI2.WinForms.Guna2Button btn_tinhluong;
+        private Guna.UI2.WinForms.Guna2Button btn_lichday;
+        private System.Windows.Forms.Panel pn_main;
     }
 }
