c4cf1bd6d843c342f544cb72697d3afc949ed8b1
Tran Minh Duc <tranminhduc04@gmail.com>
2024-03-20 20:39:31 +0700

    sadsa

--- Hỗ Trợ GV/LichDay.cs
+++ Hỗ Trợ GV/LichDay.cs
@@ -18,18 +18,12 @@ namespace Hỗ_Trợ_GV
         private const int SOCA = 4;
         // Số ngày trong tuần
         private const int SONGAY = 7;
-        // Ngày bắt đầu trong tuần
-        private DateTime startOfWeek;
-        // Ngày kết thúc trong tuần
-        private DateTime endOfWeek;
         public LichDay()
         {
             InitializeComponent();
             currentDate = DateTime.Today;
             DisplayWeekInterval();
             GetCell();
-            MC_chonngay.FirstDayOfWeek = Day.Monday;
-            BTN_homnay.Click += BTN_homnay_Click;
         }
         
         // Thêm các cell vào tổng có 28 cell
@@ -41,111 +35,47 @@ namespace Hỗ_Trợ_GV
                 {
                     UserControlBlank ucblank = new UserControlBlank();
                     ucblank.setCa(i);
-                    ucblank.setCurrentDate(startOfWeek.AddDays(j - 1));
+                    ucblank.setCurrentDate(currentDate.AddDays(j - 1));
                     FLP_shiftcontainer.Controls.Add(ucblank);
                 }
             }
         }
+
         // Hiện khoảng tuần giữa nút tuần tới và tuần rồi
         private void DisplayWeekInterval()
         {
             DayOfWeek currentDayOfWeek = currentDate.DayOfWeek;
             int daysToAdd = currentDayOfWeek == DayOfWeek.Sunday ? 6 : (int)currentDayOfWeek - 1;
-            startOfWeek = currentDate.AddDays(-daysToAdd);
-            endOfWeek = startOfWeek.AddDays(6);
-            int daysInMonth = DateTime.DaysInMonth(startOfWeek.Year, startOfWeek.Month);
-
+            DateTime startOfWeek = currentDate.AddDays(-daysToAdd);
+            DateTime endOfWeek = startOfWeek.AddDays(6);
             LB_weekinterval.Text = $"{startOfWeek.ToString("dd/MM/yyyy")} - {endOfWeek.ToString("dd/MM/yyyy")}";
-            for (int i = 0; i < 7; i++)
-            {
-                DateTime currentDay = startOfWeek.AddDays(i);
-                string labelText = $"({currentDay.Day}/{currentDay.Month})";
-
-                switch (i)
-                {
-                    case 0:
-                        LB_d1.Text = labelText;
-                        break;
-                    case 1:
-                        LB_d2.Text = labelText;
-                        break;
-                    case 2:
-                        LB_d3.Text = labelText;
-                        break;
-                    case 3:
-                        LB_d4.Text = labelText;
-                        break;
-                    case 4:
-                        LB_d5.Text = labelText;
-                        break;
-                    case 5:
-                        LB_d6.Text = labelText;
-                        break;
-                    case 6:
-                        LB_d7.Text = labelText;
-                        break;
-                }
-            }
+           
+            LB_d1.Text = $"({startOfWeek.Day.ToString()}/{startOfWeek.Month.ToString()})";
+            LB_d2.Text = $"({(startOfWeek.Day + 1).ToString()}/{startOfWeek.Month.ToString()})";
+            LB_d3.Text = $"({(startOfWeek.Day + 2).ToString()}/{startOfWeek.Month.ToString()})";
+            LB_d4.Text = $"({(startOfWeek.Day + 3).ToString()}/{startOfWeek.Month.ToString()})";
+            LB_d5.Text = $"({(startOfWeek.Day + 4).ToString()}/{startOfWeek.Month.ToString()})";
+            LB_d6.Text = $"({(startOfWeek.Day + 5).ToString()}/{startOfWeek.Month.ToString()})";
+            LB_d7.Text = $"({endOfWeek.Day.ToString()}/{endOfWeek.Month.ToString()})";
         }
         
         // Qua tuần kế tiếp
         private void Btn_tuantruoc_Click(object sender, EventArgs e)
         {
-            MC_chonngay.DateChanged -= MC_chonngay_DateChanged;
-            DateTime previousDate = currentDate;
             currentDate = currentDate.AddDays(-7);
             DisplayWeekInterval();
-            if (currentDate != previousDate)
-            {
-                FLP_shiftcontainer.Controls.Clear();
-                GetCell();
-                MC_chonngay.SelectionStart = startOfWeek;
-            }
-            MC_chonngay.DateChanged += MC_chonngay_DateChanged;
+
+            FLP_shiftcontainer.Controls.Clear();
+            GetCell();
         }
 
         // Quay lại tuần rồi
         private void Btn_tuansau_Click(object sender, EventArgs e)
         {
-            MC_chonngay.DateChanged -= MC_chonngay_DateChanged;
-            DateTime previousDate = currentDate;
-            currentDate = currentDate.AddDays(7); 
-            DisplayWeekInterval();
-            if (currentDate != previousDate)
-            {
-                FLP_shiftcontainer.Controls.Clear();
-                GetCell();
-                MC_chonngay.SetSelectionRange(startOfWeek, endOfWeek);
-            }
-            MC_chonngay.DateChanged += MC_chonngay_DateChanged;
-        }
-
-        private void MC_chonngay_DateChanged(object sender, DateRangeEventArgs e)
-        {
-            currentDate = MC_chonngay.SelectionStart;
+            currentDate = currentDate.AddDays(7);
             DisplayWeekInterval();
             FLP_shiftcontainer.Controls.Clear();
             GetCell();
-        }
-
-        private void BTN_homnay_Click(object sender, EventArgs e)
-        {
-            MC_chonngay.DateChanged -= MC_chonngay_DateChanged;
-            DateTime previousDate = currentDate; 
-            currentDate = DateTime.Today;
-            DayOfWeek currentDayOfWeek = currentDate.DayOfWeek;
-            int daysToAdd = currentDayOfWeek == DayOfWeek.Sunday ? 0 : (int)DayOfWeek.Monday - (int)currentDayOfWeek;
-            startOfWeek = currentDate.AddDays(daysToAdd);
-            endOfWeek = startOfWeek.AddDays(6);
-
-            DisplayWeekInterval();
-            if (currentDate != previousDate)
-            {
-                FLP_shiftcontainer.Controls.Clear();
-                GetCell();
-                MC_chonngay.SetSelectionRange(startOfWeek, endOfWeek);
-            }
-            MC_chonngay.DateChanged += MC_chonngay_DateChanged;
         }
     }
 }
